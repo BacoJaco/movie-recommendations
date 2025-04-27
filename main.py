115feda924edab3b5e8499e734ae17f5b597279a
Jacob McLaughlin <mclaughlinjacob2005@gmail.com>
2025-04-26 19:08:22 -0700

    Slightly changed inital API test

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ base_url = "http://www.omdbapi.com/"
 api_key = "&apikey=738d3f40"
 
 def get_movie_info(name):
-    url = f"{base_url}?t=Oppenheimer{api_key}"
+    url = f"{base_url}?t={name}{api_key}"
     response = requests.get(url)
     
     if response.status_code == 200:
-        movie_data = response.json()
-        print(movie_data)
+        return response.json()
     else:
         print(f"Failed to retreive data {response.status_code}")
 
 movie_name = "Oppenheimer"
-get_movie_info(movie_name)
+movie_info = get_movie_info(movie_name)
+
+if movie_info:
+    print(f"{movie_info["Title"]}")
